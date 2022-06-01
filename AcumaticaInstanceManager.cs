6e96278fd0210aad8b74f662744607c5ae72ce8c
Evgeny Afanasiev <evgeny.afanasiev@acumatica.com>
2022-06-01 13:35:08 +0300

    Listing of Acumatica Builds on S3 --lb Command with optional Acumatica major version

---
+++ AcumaticaInstanceManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Web.Administration;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+//using System.ServiceModel.Channels;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+
+namespace AcumaticaInstanceManager
+{
+    internal class AcumaticaInstanceManager
+    {
+        public AcumaticaInstanceManager()
+        {
+
+        }
+
+        internal static List<string> ListBuilds(string majorVersion = null)
+        {
+            Regex MajorVersion20Plus = new Regex(@"/2[0-9].[1-2]/", RegexOptions.IgnoreCase);
+            List<string> listOfBuilds = new List<string>();
+            using (var s3Client = new AmazonS3Client(new AnonymousAWSCredentials(), new AmazonS3Config { ServiceURL = "http://s3.amazonaws.com/" }))
+            {
+                ListObjectsV2Request request = new ListObjectsV2Request
+                {
+                    BucketName = "acumatica-builds",
+                    MaxKeys = 1000,
+                    Prefix = string.IsNullOrEmpty(majorVersion) ? "" : "builds/" + majorVersion
+                };
+                ListObjectsV2Response response;
+                do
+                {
+                    response = s3Client.ListObjectsV2(request);
+                    listOfBuilds.AddRange(response.S3Objects.Where(S3o => 
+                                                                    S3o.Key.Contains("AcumaticaERPInstall.msi") && 
+                                                                    MajorVersion20Plus.IsMatch(S3o.Key) &&
+                                                                    !S3o.Key.Contains("hotfix") &&
+                                                                    !S3o.Key.Contains("hidden")
+                                                                    ).ToList().Select(S3o => S3o.Key.Split('/')[!S3o.Key.Contains("preview") ? 1 : 2]+ '|' +S3o.Key.Split('/')[!S3o.Key.Contains("preview") ? 2 : 3]));
+                    request.ContinuationToken = response.NextContinuationToken;
+
+                } while (response.IsTruncated);
+            }
+            return listOfBuilds;                        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AcumaticaInstanceManager
@@ -10,6 +14,39 @@ namespace AcumaticaInstanceManager
     {
         static void Main(string[] args)
         {
+            if (args.Length > 0) {
+                int listBuildsCommandIndex = Array.IndexOf(args, "--lb");
+                if (listBuildsCommandIndex >= 0){
+                    ListBuilds(args.Length > listBuildsCommandIndex+1 ? CheckMajorVersionInput(args[listBuildsCommandIndex+1]) : null);
+                }
+            }
+        }
+
+        private static string CheckMajorVersionInput(string majorVersionRaw)
+        {
+            string majorVersion = majorVersionRaw.ToUpper().Replace(" ", string.Empty).Replace("R", ".");
+            if (!majorVersion.Contains("--") && majorVersion.Length < 7){
+                Regex MajorVersion20Plus = new Regex(@"2[0-9].[1-2]", RegexOptions.IgnoreCase);
+                string checkedVersion = MajorVersion20Plus.Match(majorVersion).Value;
+                if (!string.IsNullOrEmpty(checkedVersion)) { return checkedVersion; } else { Console.WriteLine("Acumatica Version Version could not be parsed in " + majorVersionRaw); return null;}
+            } else { return null;}
+        }
+
+        static void ListBuilds(string majorVersion = null)
+        {
+            List<string> builds = AcumaticaInstanceManager.ListBuilds(majorVersion);
+            if (builds.Count == 0 && !string.IsNullOrEmpty(majorVersion)) { Console.WriteLine($"No Acumatica ERP builds found for {majorVersion} Version");}
+            foreach (string build in builds.OrderBy(r=>r))
+            {
+                if (string.IsNullOrEmpty(majorVersion) || majorVersion != build.Split('|')[0])
+                {
+                    majorVersion = build.Split('|')[0];
+                    Console.WriteLine();
+                    Console.WriteLine("- Acumatica ERP 20" + majorVersion.Replace('.', 'R'));
+                }
+                Console.WriteLine("|--" + build.Split('|')[1]);
+            }
+            Console.ReadLine();
         }
     }
 }
